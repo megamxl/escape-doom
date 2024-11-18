19a45ce837af31f4a11da48ab4c0d3186896f0e2
Thomas Winter <thommy2002@outlook.com>
2024-11-18 17:57:14 +0100

    refactor: Moved leaderboard to \gameSession

--- next-frontend/app/utils/api/leaderboard/useGetLeaderboard.ts
+++ next-frontend/app/utils/api/leaderboard/useGetLeaderboard.ts
@@ -1,5 +1,5 @@
 import {useQuery} from "@tanstack/react-query";
-import {getLeaderboard} from "@/app/api/lectorPortal/leaderboards";
+import {getLeaderboard} from "@/app/api/gameSession/leaderboards";
 
 export const useGetLeaderboard = (lobbyId: number) => {
     return useQuery({
